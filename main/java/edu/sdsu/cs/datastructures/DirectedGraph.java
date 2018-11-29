adf45e24264b238627946d33d5dad391188fdce4
Cody Edgington <cody.edgington@gmail.com>
2018-11-28 20:55:34 -0800

    DirectedGraph proggress

--- main/java/edu/sdsu/cs/datastructures/DirectedGraph.java
+++ main/java/edu/sdsu/cs/datastructures/DirectedGraph.java
@@ -4,34 +4,12 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class DirectedGraph<V> implements IGraph<V> {
-
-    List<V> adjacencyList = new LinkedList<>();
+    TreeMap<V, LinkedList<V>> adjacencyList = new TreeMap<V, LinkedList<V>>();
 
     public DirectedGraph(){
     }
 
     public DirectedGraph(IGraph<V> data){
-    }
-
-    public class Vertex {
-        public V name;
-        List<V> adjacencyList = new LinkedList();
-
-        public Vertex(V name){
-            this.name = name;
-        }
-    }
-
-    public class Edge {
-        public Vertex start;
-        public Vertex end;
-        public double weight;
-
-        public Edge(Vertex source, Vertex destination, double cost){
-            this.start = source;
-            this.end = destination;
-            this.weight = cost;
-        }
     }
 
     public String toString(){
@@ -40,21 +18,24 @@ public class DirectedGraph<V> implements IGraph<V> {
 
     @Override
     public void add(V vertexName) {
-        if (!adjacencyList.contains(vertexName)) {
-            Vertex v = new Vertex(vertexName);
+        if (!adjacencyList.containsKey(vertexName)) {
+            adjacencyList.put(vertexName, new LinkedList<V>());
         }
+
+
     }
 
     @Override
     public void connect(V start, V destination) {
-        if (!adjacencyList.contains(start) || !adjacencyList.contains(destination)){
-            throw new java.util.NoSuchElementException("Either the source or destination does not exist");
+        if (!adjacencyList.containsKey(start)){
+            throw new java.util.NoSuchElementException("There is no element matching the start, add first");
         }
+        adjacencyList.get(start).add(destination);
     }
 
     @Override
     public void clear() {
-
+        adjacencyList = new TreeMap<V, LinkedList<V>>();
     }
 
     @Override
@@ -64,22 +45,22 @@ public class DirectedGraph<V> implements IGraph<V> {
 
     @Override
     public void disconnect(V start, V destination) {
-
+        adjacencyList.get(start).remove(destination);
     }
 
     @Override
     public boolean isConnected(V start, V destination) {
-        return false;
+        return adjacencyList.get(start).contains(destination);
     }
 
     @Override
     public Iterable<V> neighbors(V vertexName) {
-        return null;
+        return adjacencyList.get(vertexName);
     }
 
     @Override
     public void remove(V vertexName) {
-
+        adjacencyList.remove(vertexName);
     }
 
     @Override
@@ -89,16 +70,15 @@ public class DirectedGraph<V> implements IGraph<V> {
 
     @Override
     public int size() {
-        return 0;
+        return adjacencyList.size();
     }
 
     @Override
     public Iterable<V> vertices() {
-        return null;
+        return (List)adjacencyList;
     }
 
     @Override
     public IGraph<V> connectedGraph(V origin) {
         return null;
     }
-}
